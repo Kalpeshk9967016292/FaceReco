b02c137af37a50e0282b476b72609a7a95aac384
Kalpesh Kalekar <kalpeshk9967016292@gmail.com>
2015-01-16 14:46:11 +0530

    Added

---
+++ CameraCapture/CameraCapture.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace CameraCapture
+{
+    partial class CameraCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CamImageBox = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CamImageBox
+            // 
+            this.CamImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CamImageBox.Location = new System.Drawing.Point(19, 6);
+            this.CamImageBox.Name = "CamImageBox";
+            this.CamImageBox.Size = new System.Drawing.Size(495, 372);
+            this.CamImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CamImageBox.TabIndex = 2;
+            this.CamImageBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(439, 384);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 35);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 396);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Faces in Frame :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(30, 412);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(124, 104);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox1.TabIndex = 6;
+            this.imageBox1.TabStop = false;
+            // 
+            // CameraCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 524);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.CamImageBox);
+            this.Name = "CameraCapture";
+            this.Text = "Camera Output";
+            this.Load += new System.EventHandler(this.CameraCapture_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox CamImageBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Emgu.CV.UI.ImageBox imageBox1;
+    }
+}
+
---
+++ CameraCapture/CameraCapture.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+namespace CameraCapture
+{
+    public partial class CameraCapture : Form
+    {
+        public CameraCapture()
+        {
+            InitializeComponent();
+        }
+
+        private Capture capture;
+        private bool captureInProgress;
+        private HaarCascade haar;
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            Image<Bgr, Byte> ImageFrame = capture.QueryFrame();
+
+            if (ImageFrame != null)
+            {
+                Image<Gray, byte> grayframe = ImageFrame.Convert<Gray, byte>();
+                var faces = grayframe.DetectHaarCascade(haar, 1.2, 2, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(50, 50))[0];
+
+                if (faces.Length > 0)
+                {
+                    foreach (var face in faces)
+                    {
+                        //int i=0;
+                        ImageFrame.Draw(face.rect, new Bgr(Color.Red), 3);
+                        //ImageFrame.Save("Face"+i.ToString()+".jpg");
+                        label2.Text= "\n Face Detected : " + faces.Length.ToString();
+
+                       // i = i + 1;
+                        
+
+                    }
+                }
+            }
+
+            CamImageBox.Image = ImageFrame;
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                try
+                {
+                    capture = new Capture();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+
+            if (capture != null)
+            {
+                if (captureInProgress)
+                {
+                    btnStart.Text = "Start";
+                    Application.Idle -= ProcessFrame;
+                }
+                else
+                {
+                    btnStart.Text = "Stop";
+                    Application.Idle += ProcessFrame;
+                }
+
+                captureInProgress = !captureInProgress;
+            }
+        }
+
+        private void ReleseData()
+        {
+            if (capture != null)
+            {
+                capture.Dispose();
+            }
+        }
+
+        private void CameraCapture_Load(object sender, EventArgs e)
+        {
+            haar = new HaarCascade("haarcascade.xml");
+        }
+    }
+}
